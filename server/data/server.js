2706e6ef97283571c9145bfc706b7cd4ec1a05d9
juan cruz benitez arce <juancruzbeniteza@gmail.com>
2024-01-16 20:37:17 -0300

    Desafio

--- server/data/server.js
+++ server/data/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
 const ProductFsManager = require('./Fs/files/products.fs.js');
 const UserFsManager = require('./Fs/files/users.fs.js');
 const ProductMemoryManager = require('./memory/products.memory.js');
@@ -7,11 +11,24 @@ const UserMemoryManager = require('./memory/users.memory.js');
 const CartFsManager = require('./Fs/files/carts.fs.js');
 const CartMemoryManager = require('./memory/cart.memory.js');
 
-
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const PORT = 8080;
 
 app.use(bodyParser.json());
+
+// Set up Handlebars
+const hbs = exphbs.create({ defaultLayout: 'main' });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Set up body parser middleware
+app.use(bodyParser.json());
+
+// Set up static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 const productManager = process.env.USE_MEMORY_MANAGER === 'true' ? new ProductMemoryManager() : new ProductFsManager('./Fs/files/productos.json');
 const userManager = process.env.USE_MEMORY_MANAGER === 'true' ? new UserMemoryManager() : new UserFsManager('./Fs/files/users.json');
@@ -59,10 +76,6 @@ app.get('/api/users/:uid', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 // Carts Endpoints
 app.post('/api/carts', async (req, res) => {
   const newCart = await cartManager.create();
@@ -86,10 +99,28 @@ app.post('/api/carts/:cid/product/:pid', async (req, res) => {
   const updatedCart = await cartManager.addProductToCart(cartId, productId, quantity);
 
   if (updatedCart) {
+    io.emit('productUpdated', { cartId, updatedCart });
     res.json({ success: true, response: updatedCart });
   } else {
     res.status(404).json({ success: false, message: 'not found!' });
   }
 });
 
-// ...
+// Set up WebSocket
+io.on('connection', (socket) => {
+  console.log('A user connected');
+});
+
+// Set up views router
+app.get('/realTimeProducts', async (req, res) => {
+  const products = await productManager.read();
+  res.render('realTimeProducts', { products });
+});
+
+// Set up static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
